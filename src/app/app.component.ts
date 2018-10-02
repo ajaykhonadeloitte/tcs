93bbec81650e608e51cdf2258d1ce120e5c63d9f
ajaykhonadeloitte <akhona@deloitte.com>
2018-10-02 12:23:52 +0530

    Home Page Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +9,9 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'app';
+  constructor(){
+
+  }
   scroll(){
     console.log('Scrolled!');
   }
